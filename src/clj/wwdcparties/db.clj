050e61c60060c5e70e5f439dc23c5a2ed8f9d360
Geoff Pado <geoff@pado.name>
2015-01-09 18:10:22 -0600

    Sanitize party information.

--- src/clj/wwdcparties/db.clj
+++ src/clj/wwdcparties/db.clj
@@ -1,13 +1,14 @@
 (ns wwdcparties.db
   (:require [environ.core :refer [env]]
-            [com.ashafa.clutch :as clutch]))
+            [com.ashafa.clutch :as clutch]
+            [wwdcparties.party :as party]))
 
 (def db
   (clutch/get-database (env :wwdc-parties-db)))
 
 (defn get-all-parties []
-  (map :value
-    (clutch/get-view db "parties" "list")))
+  (map party/sanitized (map :value
+    (clutch/get-view db "parties" "list"))))
 
 (defn add-party [party]
-	(clutch/put-document db party))
+	(clutch/put-document db (party/sanitized party)))
---
+++ src/clj/wwdcparties/party.clj
@@ -0,0 +1,9 @@
+(ns wwdcparties.party)
+
+(defn sanitized [party]
+	(select-keys party
+		[:name :slug :start_time :end_time
+		:description :shortcode :event_url
+		:street_address :location :twitter
+		:sponsor_url :sponsor_name
+		:types :meta]))
