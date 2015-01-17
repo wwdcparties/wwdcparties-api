c0b9e200f38e43a54906fa0e14ac1ae1a9364f3f
Geoff Pado <geoff@pado.name>
2015-01-17 17:59:04 -0600

    Clean up to remove ClojureScript.

--- project.clj
+++ project.clj
@@ -3,7 +3,6 @@
   :url "http://example.com/FIXME"
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2665"]
                  [environ "1.0.0"]
                  [com.ashafa/clutch "0.4.0"]
                  [compojure "1.3.1"]
@@ -11,33 +10,11 @@
                  [ring/ring-json "0.3.1"]
                  [ring-cors "0.1.6"]]
 
-  :node-dependencies [[source-map-support "0.2.8"]]
-
-  :plugins [[lein-cljsbuild "1.0.4"]
-            [lein-npm "0.4.0"]
-            [lein-environ "1.0.0"]
+  :plugins [[lein-environ "1.0.0"]
             [lein-ring "0.8.13"]]
 
-  :source-paths ["src/clj" "src/cljs" "target/classes"]
+  :source-paths ["src"]
   :main ^:skip-aot wwdcparties.core
   :aot :all
   :uberjar-name "wwdcparties.jar"
-  :ring {:handler wwdcparties.api/api :port 8080}
-
-  :clean-targets ["out/wwdc_parties" "wwdc_parties.js" "wwdc_parties.min.js"]
-
-  :cljsbuild {
-    :builds [{:id "dev"
-              :source-paths ["src"]
-              :compiler {
-                :output-to "wwdc_parties.js"
-                :output-dir "out"
-                :optimizations :none
-                :cache-analysis true                
-                :source-map true}}
-             {:id "release"
-              :source-paths ["src"]
-              :compiler {
-                :output-to "wwdc_parties.min.js"
-                :pretty-print false              
-                :optimizations :advanced}}]})
+  :ring {:handler wwdcparties.api/api :port 8080})
--- src/wwdcparties/core.clj
+++ src/wwdcparties/core.clj
@@ -1,4 +1,5 @@
-(ns wwdcparties.core)
+(ns wwdcparties.core
+	(:gen-class))
 
 (defn -main []
 	(println "Hello, world!"))
