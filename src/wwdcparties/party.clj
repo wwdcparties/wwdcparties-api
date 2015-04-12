24efaec978504523164b52ffda3d27158043493b
Geoff Pado <geoff@pado.name>
2015-04-12 10:52:55 -0700

    Include Twitter handle in the API.

--- src/wwdcparties/party.clj
+++ src/wwdcparties/party.clj
@@ -3,7 +3,8 @@
 (def party-keys [:description :slug :meta :name :type
                  :start_time :excerpt :sponsor_url
                  :types :end_time :shortcode :event_url
-                 :street_address :sponsor_name :location])
+                 :street_address :sponsor_name :location
+                 :twitter_handle])
 
 (defn tidy [party] (select-keys party party-keys))
 
