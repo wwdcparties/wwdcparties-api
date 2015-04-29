bb928744d57a8d2d35662ee70d79d2f04f3838e2
Geoff Pado <geoff@pado.name>
2015-04-28 22:04:00 -0700

    Call from Foursquare API.

--- project.clj
+++ project.clj
@@ -9,7 +9,8 @@
                  [compojure "1.3.1"]
                  [ring/ring-core "1.3.2"]
                  [ring/ring-json "0.3.1"]
-                 [com.cemerick/friend "0.2.1"]]
+                 [com.cemerick/friend "0.2.1"]
+                 [clj-http "1.0.1"]]
 
   :plugins [[lein-environ "1.0.0"]
             [lein-ring "0.8.13"]]
---
+++ src/wwdcparties/foursquare.clj
@@ -0,0 +1,33 @@
+(ns wwdcparties.foursquare
+  (:require [clj-http.client :as http]
+            [clojure.data.json :refer [read-json]]
+            [environ.core :refer [env]]))
+
+(def foursquare-client-id (env :wwdc-parties-foursquare-id))
+(def foursquare-secret (env :wwdcparties-foursquare-secret))
+
+(defn suggestions-url [query]
+  (str "https://api.foursquare.com/v2/venues/suggestcompletion"
+       "?v=20150428"
+       "&near=San%20Francisco,%20CA"
+       "&client_id=" foursquare-client-id
+       "&client_secret=" foursquare-secret
+       "&query=" query))
+
+(defn suggestions-json [query]
+  (-> query
+      suggestions-url
+      http/get
+      :body
+      read-json))
+
+(defn parse-suggestion [json]
+  {:name (:name json)
+   :address (:address (:location json))})
+
+(defn suggestions [query]
+  (map parse-suggestion 
+       (-> query
+           suggestions-json
+           :response
+           :minivenues)))
