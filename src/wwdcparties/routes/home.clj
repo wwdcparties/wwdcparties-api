e110002e5a43b3eed9445c2d46c8370f0d1c84f4
Geoff Pado <geoff@pado.name>
2016-02-16 22:16:49 -0800

    Fix info pages.

--- src/wwdcparties/routes/home.clj
+++ src/wwdcparties/routes/home.clj
@@ -5,10 +5,15 @@
             [wwdcparties.render.index :as index]
             [hiccup.page :refer [html5]]
             [wwdcparties.api.db :as db]
-            [wwdcparties.model.party :as party]))
+            [wwdcparties.model.party :as party]
+            [wwdcparties.render.info :as info]))
 
 (defn index-page []
   (html5 (index/page (sort-by :start_time (map party/from-json (db/parties))))))
 
+(defn info-page [slug]
+  (html5 (info/page (party/from-json (db/parties slug)))))
+
 (defroutes home-routes
-  (GET "/" [] (index-page)))
+  (GET "/" [] (index-page))
+  (GET "/parties/:slug" [slug] (info-page slug)))
