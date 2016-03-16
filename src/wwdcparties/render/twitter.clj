6328d3dbea0a698c9e06a22cf027a941f8d8be3b
Geoff Pado <geoff@pado.name>
2016-03-15 21:08:55 -0700

    Remove app-related stuff.

--- src/wwdcparties/render/twitter.clj
+++ src/wwdcparties/render/twitter.clj
@@ -5,21 +5,5 @@
   (list [:meta {:name "twitter:card" :content "summary"}]
         [:meta {:name "twitter:site" :content "@wwdcparties"}]
         [:meta {:name "twitter:title" :content (:name party)}]
-        [:meta {:name "twitter:description" :content (:excerpt party)}]
-        [:meta {:name "twitter:app:country" :content "US"}]
-        [:meta {:name "twitter:app:name:iphone" :content "WWDC Parties"}]
-        [:meta {:name "twitter:app:id:iphone" :content "956296991"}]
-        [:meta {:name "twitter:app:url:iphone" :content (str "wwdcparties://parties/" (:slug party))}]
-        [:meta {:name "twitter:app:name:ipad" :content "WWDC Parties"}]
-        [:meta {:name "twitter:app:id:ipad" :content "956296991"}]
-        [:meta {:name "twitter:app:url:ipad" :content (str "wwdcparties://parties/" (:slug party))}]))
+        [:meta {:name "twitter:description" :content (:excerpt party)}]))
 
-(def app
-  (list
-   [:meta {:name "twitter:card" :content "app"}]
-   [:meta {:name "twitter:site" :content "@wwdcparties"}]
-   [:meta {:name "twitter:description" :content "View the latest parties at Apple's developer conference."}]
-   [:meta {:name "twitter:app:name:iphone" :content "WWDC Parties"}]
-   [:meta {:name "twitter:app:id:iphone" :content "956296991"}]
-   [:meta {:name "twitter:app:name:ipad" :content "WWDC Parties"}]
-   [:meta {:name "twitter:app:id:ipad" :content "956296991"}]))
