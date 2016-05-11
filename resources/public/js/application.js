34cb86c6c14a83ee9ce5f8d81b6949988142b7c0
Geoff Pado <geoff@pado.name>
2016-05-10 19:55:57 -0700

    Implement filter by URL params.

--- resources/public/js/application.js
+++ resources/public/js/application.js
@@ -12,10 +12,35 @@ function activeFilters() {
 function applyFilters() {
     var filters = activeFilters();
     listingElementsArray().forEach(function (element) {
-        var elementClasses = element.className.split(" ");
+        var elementClasses = element.className.split(' ');
         var matchingClasses = elementClasses.filter(function (elementClass) { return filters.indexOf(elementClass) != -1; });
         var matchesFilters = matchingClasses.length == filters.length;
-        element.style.display = (matchesFilters ? "list-item" : "none");
+        element.style.display = (matchesFilters ? 'list-item' : 'none');
+    });
+    var filtersParam = filters.join(',');
+    if (filtersParam) {
+        window.history.replaceState(null, "", window.location.pathname + "?filter=" + filtersParam);
+    }
+    else {
+        window.history.replaceState(null, "", window.location.pathname);
+    }
+}
+function setFiltersFromParam(filtersParam) {
+    var filters = filtersParam.split(',');
+    var filterElements = filterElementsArray();
+    filterElements.forEach(function (element) {
+        var elementFilter = element.getAttribute('data-filter');
+        element.checked = (filters.indexOf(elementFilter) !== -1);
     });
 }
+function getQueryString(field, url) {
+    var href = url ? url : window.location.href;
+    var reg = new RegExp('[?&]' + field + '=([^&#]*)', 'i');
+    var string = reg.exec(href);
+    return string ? string[1] : null;
+}
+window.addEventListener('load', function (event) {
+    setFiltersFromParam(getQueryString('filter'));
+    applyFilters();
+});
 //# sourceMappingURL=application.js.map
--- resources/typescript/application.ts
+++ resources/typescript/application.ts
@@ -15,9 +15,37 @@ function activeFilters() :string[] {
 function applyFilters() {
 	var filters = activeFilters();
 	listingElementsArray().forEach(element => {
-		var elementClasses = element.className.split(" ");
+		var elementClasses = element.className.split(' ');
 		var matchingClasses = elementClasses.filter(elementClass => filters.indexOf(elementClass) != -1);
 		var matchesFilters = matchingClasses.length == filters.length;
-		element.style.display = (matchesFilters ? "list-item" : "none");
+		element.style.display = (matchesFilters ? 'list-item' : 'none');
+	});
+	
+	var filtersParam = filters.join(',');
+	if (filtersParam) {
+		window.history.replaceState(null, "", window.location.pathname + "?filter=" + filtersParam);
+	} else {
+		window.history.replaceState(null, "", window.location.pathname);
+	}
+}
+
+function setFiltersFromParam(filtersParam: string) {
+	var filters = filtersParam.split(',');
+	var filterElements = filterElementsArray();
+	filterElements.forEach(element => {
+		var elementFilter = element.getAttribute('data-filter');
+		element.checked = (filters.indexOf(elementFilter) !== -1);  
 	});
 }
+
+function getQueryString(field: string, url?: string) {
+    var href = url ? url : window.location.href;
+    var reg = new RegExp( '[?&]' + field + '=([^&#]*)', 'i' );
+    var string = reg.exec(href);
+    return string ? string[1] : null;
+}
+
+window.addEventListener('load', event => {
+	setFiltersFromParam(getQueryString('filter'));
+	applyFilters();
+});
